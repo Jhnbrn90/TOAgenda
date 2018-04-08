fc95f987c0ffc6b96618ea980b6132f08a5d432f
John <jbraunnl@gmail.com>
2018-04-08 10:51:52 +0200

    Responsive table on mobile devices

--- resources/views/admin/appointments/all.blade.php
+++ resources/views/admin/appointments/all.blade.php
@@ -13,39 +13,41 @@
 
     <h2>Alle afspraken ({{ $appointments->total() }})</h2>
 
-    <table class="table table-striped table-hover">
-        <thead>
-            <tr>
-                <th>Datum</th>
-                <th>Lesuur</th>
-                <th>Titel</th>
-                <th>Docent</th>
-                <th>Klas</th>
-                <th>Status</th>
-                <th>Aangevraagd</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($appointments as $appointment)
-            <tr>
-                <td>{{ $appointment->date }}</td>
-                <td>{{ $appointment->period }}</td>
-                <td>{{ $appointment->title }}</td>
-                <td>{{ $appointment->creator->name }}</td>
-                <td>{{ $appointment->class }}</td>
-                <td><a href="/admin/appointment/{{ $appointment->id }}">
+    <div class="table-responsive">
+        <table class="table table-striped tab   le-hover">
+            <thead>
+                <tr>
+                    <th>Datum</th>
+                    <th>Lesuur</th>
+                    <th>Titel</th>
+                    <th>Docent</th>
+                    <th>Klas</th>
+                    <th>Status</th>
+                    <th>Aangevraagd</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($appointments as $appointment)
+                <tr>
+                    <td>{{ $appointment->date }}</td>
+                    <td>{{ $appointment->period }}</td>
+                    <td>{{ $appointment->title }}</td>
+                    <td>{{ $appointment->creator->name }}</td>
+                    <td>{{ $appointment->class }}</td>
+                    <td><a href="/admin/appointment/{{ $appointment->id }}">
                     @switch($appointment->accepted) @case(2)
-                    <button class="btn btn-warning">Geweigerd</button> @break @case(1)
+                    <button class="btn btn-danger">Geweigerd</button> @break @case(1)
                     <button class="btn btn-success">Geaccepteerd</button> @break @default
                     <button class="btn btn-info">Open</button> @endswitch
                 </a>
-                </td>
+                    </td>
 
-                <td>{{ $appointment->created_at->format('d-m-Y') }}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+                    <td>{{ $appointment->created_at->format('d-m-Y') }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
     {{ $appointments->links() }}
 </div>
